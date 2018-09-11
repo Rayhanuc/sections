8ee082b8f92c12719cf4f4cfb44b2ca75d7e0ccd
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2018-09-12 02:02:30 +0600

    2 class added
    Class-30, Class-31 added

---
+++ parallax/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	var $ = skrollr.init({
+    		render: function(data){
+    			//debugging - Log the current scroll position.
+    			//console.log(data.curTop);
+    		}
+    	});
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
