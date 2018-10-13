c7fb9881e4acc7bb4c5223f9ff4b18b47d64f8af
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2018-10-14 02:13:33 +0600

    class-46
    class-46 trailers-slider

---
+++ trailers-slider/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+    	$(".featured-trailors-carousel").owlCarousel({
+    		items: 1,
+    		loop: true,
+    		autoplay: false,
+    		nav: false,
+    		dots: true
+    	});
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
