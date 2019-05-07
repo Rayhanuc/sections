ce1f298e1ab7ce8b73fd08bd77ccda5af9a4f940
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-05-07 15:19:53 +0600

    Sections
    Class 57: amazing-testimonial-section

---
+++ 57.amazing-testimonial-section/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+		const container = document.querySelector('.testimonial-author-name');
+		const ps = new PerfectScrollbar(container);
+
+		// or just with selector string
+		const ps = new PerfectScrollbar('.testimonial-author-name');
+        // $(".testimonial-author-name").perfectScrollbar();
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
